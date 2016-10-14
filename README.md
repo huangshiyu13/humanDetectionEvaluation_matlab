42f40cbe3f068c9f6edf5828cdf6aa74184b3f57
Shiyu Huang <huangsy13@gmail.com>
2016-10-14 21:14:00 +0800

    updat

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # humanDetectionEvaluation_matlab
-rt
+
+## 说明
+这是做elvaluatino的matlab的仓库
+读入groudtruth和test result，画出roc曲线
+有个config file可以配置读入的文件夹和曲线的名字和颜色
+这里只是给出个示例，具体项目可以具体有不同的写法
+为了能够成功运行，需要把https://github.com/huangshiyu13/toolbox项目并行放在一起
+
+## 程序清单
+evaluation.m 这个程序就是读入结果文件，画出roc曲线
+configure/draw.cfg 
+例子：
+../../DATA/dangerousFinal/allResizedAno
+result/syntheticDataAll_InriaBkg/bbout syntheticDataAll_InriaBkg g-
+result/syntheticDataAll/bbout syntheticDataAll m-
+result/Caltech2500/bbout Caltech2500 c--
+result/Mix_Caltech2500/bbout Mix_Caltech2500 r-
+result/Caltech1000/bbout Caltech1000 b-
+result/Mix_Caltech1000/bbout Mix_Caltech1000 b--
+
+images用于存储roc图片
+
---
+++ evaluation.m
@@ -0,0 +1,55 @@
+clear all
+close all
+p = genpath('../toolbox');
+addpath(p);
+
+fid1=fopen('configure/draw.cfg');
+groundtruth = '';
+testFiles={};
+testNames={};
+type={};
+i = 0;
+while ~feof(fid1)
+    aline=fgetl(fid1);
+    if i == 0,
+        groundtruth = aline;
+    else
+       strs = regexp(aline, ' ', 'split');
+       testFiles{i} = strs{1}; 
+       testNames{i} = strs{2};
+       type{i} = strs{3};
+    end
+    i=i+1;
+end
+fclose(fid1);
+pLoad={'lbls',{'person'},'ilbls',{'people'},'squarify',{3,.41},'hRng',[50 inf]};
+p = {};
+figure(1)
+yMin = 100;
+xMax = 0;
+grid on;
+for i = 1:size(testFiles,2)
+    [gt,dt] = bbGt( 'myLoadAll', groundtruth,testFiles{i},pLoad);
+    thr = 0.1;
+    [gt,dt] = bbGt('evalRes',gt,dt,thr,0);
+    [xs,ys] = bbGt('compRoc',gt,dt,1);
+    ys = 1 - ys;
+    hold on;
+    plot(xs,ys,type{i},'LineWidth',3);
+    yMin=min(yMin,min(ys)); 
+    xMax=max(xMax,max(xs));
+    xlabel('false positives per image','FontSize',14);
+    ylabel('miss rate','FontSize',14);
+end
+yt=[.05 .1:.1:.5 .64 .8]; ytStr=int2str2(yt*100,2);
+for i=1:length(yt), ytStr{i}=['.' ytStr{i}]; end
+set(gca,'XScale','log','YScale','log',...
+        'YTick',[yt 1],'YTickLabel',[ytStr '1'],...
+        'XMinorGrid','off','XMinorTic','off',...
+        'YMinorGrid','off','YMinorTic','off');
+title('ROC');
+xlim([0 xMax]);
+ylim([yMin, 1]);
+
+legend(testNames,'Location','sw');
+saveas(gcf,'images/testDangerous.jpg');
